28c9e3a22adb08825fa77e0dc9df8159b7945786
Prince <gbolahanhardemuyiwa@gmail.com>
2026-01-07 10:27:37 +0100

    new

---
+++ nigerian_tax_reform_bills_2024_frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import NigerianTax from './components/NigerianTax';
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <NigerianTax/>
+    </div>
+  )
+}
+
+export default App;
